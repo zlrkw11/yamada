04cca80b7c9da548c1fcf86abf8f9e7785f02867
ZL <ray040406@gmail.com>
2025-04-22 12:25:25 +1200

    feat: button component demo

---
+++ app/components/ButtonComponent.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@yamada-ui/react";
+
+const ButtonComponent = () => {
+  return <Button>Button</Button>;
+};
+
+export default ButtonComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import ButtonComponent from "./components/ButtonComponent";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+      <h1>button component:</h1>
+      <ButtonComponent />
+    </div>
+  );
 }
