c97b461f405915f2a45ca2df6b73149a92d56fa1
ZL <ray040406@gmail.com>
2025-04-23 12:19:43 +1200

    feat: testing components on the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,61 @@
-import ButtonComponent from "./components/ButtonComponent";
-import { Button } from "@yamada-ui/react";
-import Navbar from "./components/Navbar";
+import {
+  Text,
+  Heading,
+  Box,
+  Blockquote,
+  Container,
+  Image,
+  Input,
+} from "@yamada-ui/react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <Navbar />
-      <h1>button component:</h1>
-      <ButtonComponent />
-      <h1>button with customized primary color:</h1>
-      <Button variant="solid" colorScheme="primary">
-        Button
-      </Button>
-      <Button variant="outline" colorScheme="orange">
-        Outline
-      </Button>
-    </div>
+    <Box display="flex" flexDirection="column" maxW="100vw" alignItems="center">
+      <Box gap="sm" display="flex" flexDirection="column">
+        {" "}
+        <Heading
+          marginLeft="lg"
+          marginTop="lg"
+          bgClip="text"
+          bgGradient="linear(to-r, #D6D6D6, #888888)"
+        >
+          Ui example page
+        </Heading>
+        <Text marginLeft="lg">Ray Zhao</Text>
+        <Blockquote marginLeft="lg">
+          This is a section to show how blockquote component works
+        </Blockquote>{" "}
+        <Image
+          src="https://dragon-ball-official.com/assets/img/intro/intro_2.png"
+          maxW="xs"
+          marginLeft="md"
+          alt="Dragon Ball"
+        />
+        <Container
+          borderBlockColor="gray.100"
+          borderWidth="2px"
+          rounded="md"
+          marginLeft="lg"
+          maxW="lg"
+        >
+          {" "}
+          <Heading size="xl"> Dragon Ball (DRAGON BALL) </Heading>
+          <Text>
+            Dragon Ball (DRAGON BALL) is a Japanese manga work by Akira
+            Toriyama. It was serialized in Weekly Shonen Jump (Shueisha) from
+            the 51st issue of 1984 to the 25th issue of 1995. It is a long manga
+            that depicts adventure, dream, battle, friendship, etc., centered on
+            the protagonist, Son Goku, and the treasure, Dragon Ball, which can
+            grant any wish if all seven scattered balls are collected.
+          </Text>
+        </Container>
+        <Input
+          marginLeft="lg"
+          invalid
+          errorBorderColor="orange.500"
+          placeholder="custom border color"
+        />{" "}
+      </Box>
+    </Box>
   );
 }
